80ac005e31a76c6bb1de73f702d9cd224420f4b7
albarson4 <albarson4@gmail.com>
2024-03-06 03:19:14 +0000

    upload code

---
+++ README.md
@@ -0,0 +1,55 @@
+# negotiated [![Build Status](https://travis-ci.org/JoshuaWise/negotiated.svg?branch=master)](https://travis-ci.org/JoshuaWise/negotiated)
+
+This is a low-level utility for correctly parsing the HTTP content negotiation headers. It doesn't interpret the parsed values in any way, except for ensuring that they are syntactically correct.
+
+## Installation
+
+```bash
+npm install --save negotiated
+```
+
+## Usage
+
+```js
+const negotiated = require('negotiated');
+
+const best = Array.from(negotiated.languages('fr;q=0.4, ja-JP;q=0.2, de-DE;q=0.7, en;q=0.5'))
+  .reduce((a, b) => a.weight >= b.weight ? a : b);
+
+console.log(best.language); // => "de-de"
+```
+
+This package exports six functions:
+
+- `mediaTypes()`: parses the Accept header, emitting `{ type, params, weight, extensions }`
+- `charsets()`: parses the Accept-Charset header, emitting `{ charset, weight }`
+- `encodings()`: parses the Accept-Encoding header, emitting `{ encoding, weight }`
+- `languages()`: parses the Accept-Language header, emitting `{ language, weight }`
+- `transferEncodings()`: parses the TE header, emitting `{ encoding, params, weight }`
+- `parameters()`: parses the `params` and `extensions` found above, emitting `{ key, value }`
+
+Each of the exported functions takes a string as the only argument. An iterator is returned, which parses one comma-separated item at a time. If the input string is invalid (according to [RFC 7230](https://tools.ietf.org/html/rfc7230) or [RFC 7231](https://tools.ietf.org/html/rfc7231)), an error will be thrown mid-iteration.
+
+## Examples
+
+#### Iterating over the Accept-Encoding header
+
+```js
+const negotiated = require('negotiated');
+
+for (const { encoding, weight } of negotiated.encodings('gzip;q=0.5, my-custom-encoding;q=1')) {
+  if (weight > 0.8) console.log(`${encoding} is desired`);
+}
+
+// => "my-custom-encoding is desired"
+```
+
+#### Parsing media parameters
+
+```js
+const negotiated = require('negotiated');
+
+const [{ params }] = Array.from(negotiated.mediaTypes('application/json; CHARSET="utf-8"'));
+
+console.log(Array.from(negotiated.parameters(params))); // => [{ key: 'charset', value: 'utf-8' }]
+```
---
+++ test/20.parameters.js
@@ -0,0 +1,51 @@
+'use strict';
+const { expect } = require('chai');
+const { parameters, mediaTypes, transferEncodings } = require('../.');
+
+const param = (key, value) => ({ key, value });
+const parse = (iterator, ext = false) => [...parameters([...iterator][0][ext ? 'extensions' : 'params'])];
+
+describe('parameters', function () {
+	it('should throw when given improper input', function () {
+		expect(() => [...parameters(1)]).to.throw(TypeError);
+		expect(() => [...parameters({})]).to.throw(TypeError);
+		expect(() => [...parameters('foo')]).to.throw(Error);
+		expect(() => [...parameters('foo=bar')]).to.throw(Error);
+		expect(() => [...parameters('; foo;')]).to.throw(Error);
+		expect(() => [...parameters('; foo ')]).to.throw(Error);
+		expect(() => [...parameters('; foo=bar;')]).to.throw(Error);
+		expect(() => [...parameters('; foo=bar ')]).to.throw(Error);
+		expect(() => [...parameters('; foo="bar" ')]).to.throw(Error);
+	});
+	it('should correctly parse media parameters', function () {
+		expect(parse(mediaTypes('#/#; a=b'))).to.deep.equal([param('a', 'b')]);
+		expect(parse(mediaTypes('#/#; FOO=BAR'))).to.deep.equal([param('foo', 'BAR')]);
+		expect(parse(mediaTypes('#/#; FOO=" ;ba\\r\\\\\\\" "'))).to.deep.equal([param('foo', ' ;bar\\" ')]);
+		expect(parse(mediaTypes('#/#;   FOO=" ;ba\\r\\\\\\\" "  ;  *=bcde123'))).to.deep.equal([
+			param('foo', ' ;bar\\" '),
+			param('*', 'bcde123'),
+		]);
+	});
+	it('should correctly parse transfer-encoding parameters', function () {
+		expect(parse(transferEncodings('#; a  \t=  b'))).to.deep.equal([param('a', 'b')]);
+		expect(parse(transferEncodings('#; FOO = BAR'))).to.deep.equal([param('foo', 'BAR')]);
+		expect(parse(transferEncodings('#; FOO  = \t" ;ba\\r\\\\\\\" "'))).to.deep.equal([param('foo', ' ;bar\\" ')]);
+		expect(parse(transferEncodings('#; FOO=\t\t" ;ba\\r\\\\\\\" "  ;  *\t =bcde123'))).to.deep.equal([
+			param('foo', ' ;bar\\" '),
+			param('*', 'bcde123'),
+		]);
+	});
+	it('should correctly parse extension parameters', function () {
+		expect(parse(mediaTypes('#/#;q=1; a=b'), true)).to.deep.equal([param('a', 'b')]);
+		expect(parse(mediaTypes('#/#;q=1; A'), true)).to.deep.equal([param('a')]);
+		expect(parse(mediaTypes('#/#;q=1; FOO=BAR'), true)).to.deep.equal([param('foo', 'BAR')]);
+		expect(parse(mediaTypes('#/#;q=1; FOO=" ;ba\\r\\\\\\\" "'), true)).to.deep.equal([param('foo', ' ;bar\\" ')]);
+		expect(parse(mediaTypes('#/#;q=1;   FOO=" ;ba\\r\\\\\\\" "  ; X  ; YZ ;  *=bcde123; ***'), true)).to.deep.equal([
+			param('foo', ' ;bar\\" '),
+			param('x'),
+			param('yz'),
+			param('*', 'bcde123'),
+			param('***'),
+		]);
+	});
+});
